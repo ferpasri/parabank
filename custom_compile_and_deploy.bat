532dd73135bad75b5a56b59161a102e8cf67dbe0
Fernando Pastor <32359126+ferpasri@users.noreply.github.com>
2023-10-02 20:54:43 +0200

    Disable lookup and register pages

--- custom_compile_and_deploy.bat
+++ custom_compile_and_deploy.bat
@@ -1,14 +1,14 @@
-:: Prepare an Apache v9.0.71 server
-:: https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.71/bin/apache-tomcat-9.0.71-windows-x64.zip
+:: Prepare an Apache v9.0.80 server
+:: https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.80/bin/apache-tomcat-9.0.80-windows-x64.zip
 
-if exist "deploy\apache-tomcat-9.0.71" (echo "apache-tomcat-9.0.71 exists, stopping a possible execution"
+if exist "deploy\apache-tomcat-9.0.80" (echo "apache-tomcat-9.0.80 exists, stopping a possible execution"
 :: If apache exists, we are going to stop possible executions
-set "CATALINA_HOME=%~dp0\deploy\apache-tomcat-9.0.71"
-CALL "deploy\apache-tomcat-9.0.71\bin\shutdown.bat"
-) else (echo "downloading apache-tomcat-9.0.71..."
+set "CATALINA_HOME=%~dp0\deploy\apache-tomcat-9.0.80"
+CALL "deploy\apache-tomcat-9.0.80\bin\shutdown.bat"
+) else (echo "downloading apache-tomcat-9.0.80..."
 :: If apache does not exists, we download the software from the oficial link and unzip
-curl -L "https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.71/bin/apache-tomcat-9.0.71-windows-x64.zip" -o "deploy\apache-tomcat-9.0.71-windows-x64.zip" --create-dirs
-tar -xf "deploy\apache-tomcat-9.0.71-windows-x64.zip" -C "deploy"
+curl -L "https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.80/bin/apache-tomcat-9.0.80-windows-x64.zip" -o "deploy\apache-tomcat-9.0.80-windows-x64.zip" --create-dirs
+tar -xf "deploy\apache-tomcat-9.0.80-windows-x64.zip" -C "deploy"
 )
 
 :: Clean and install a new parabank war file
@@ -16,12 +16,12 @@ tar -xf "deploy\apache-tomcat-9.0.71-windows-x64.zip" -C "deploy"
 CALL mvn -Dmaven.test.skip=true clean install
 
 :: Copy the war file to apache and deploy the web server
-rmdir /s /q "deploy\apache-tomcat-9.0.71\webapps"
-mkdir "deploy\apache-tomcat-9.0.71\webapps"
-xcopy /s "target\parabank.war" "deploy\apache-tomcat-9.0.71\webapps\*"
+rmdir /s /q "deploy\apache-tomcat-9.0.80\webapps"
+mkdir "deploy\apache-tomcat-9.0.80\webapps"
+xcopy /s "target\parabank.war" "deploy\apache-tomcat-9.0.80\webapps\*"
 
-set "CATALINA_HOME=%~dp0\deploy\apache-tomcat-9.0.71"
-CALL "deploy\apache-tomcat-9.0.71\bin\startup.bat"
+set "CATALINA_HOME=%~dp0\deploy\apache-tomcat-9.0.80"
+CALL "deploy\apache-tomcat-9.0.80\bin\startup.bat"
 
 timeout /t 30
 start chrome http://localhost:8080/parabank
