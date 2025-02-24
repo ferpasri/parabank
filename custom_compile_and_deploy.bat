0b38dd91125e09ca26ef98bff12e0043f8a38c14
ferpasri <ferpasri@gmail.com>
2025-02-24 09:38:47 +0100

    Update apache tomcat 9 version

--- custom_compile_and_deploy.bat
+++ custom_compile_and_deploy.bat
@@ -1,14 +1,14 @@
-:: Prepare an Apache v9.0.80 server
-:: https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.80/bin/apache-tomcat-9.0.80-windows-x64.zip
+:: Prepare an Apache v9.0.100 server
+:: https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.100/bin/apache-tomcat-9.0.100-windows-x64.zip
 
-if exist "deploy\apache-tomcat-9.0.80" (echo "apache-tomcat-9.0.80 exists, stopping a possible execution"
+if exist "deploy\apache-tomcat-9.0.100" (echo "apache-tomcat-9.0.100 exists, stopping a possible execution"
 :: If apache exists, we are going to stop possible executions
-set "CATALINA_HOME=%~dp0\deploy\apache-tomcat-9.0.80"
-CALL "deploy\apache-tomcat-9.0.80\bin\shutdown.bat"
-) else (echo "downloading apache-tomcat-9.0.80..."
+set "CATALINA_HOME=%~dp0\deploy\apache-tomcat-9.0.100"
+CALL "deploy\apache-tomcat-9.0.100\bin\shutdown.bat"
+) else (echo "downloading apache-tomcat-9.0.100..."
 :: If apache does not exists, we download the software from the oficial link and unzip
-curl -L "https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.80/bin/apache-tomcat-9.0.80-windows-x64.zip" -o "deploy\apache-tomcat-9.0.80-windows-x64.zip" --create-dirs
-tar -xf "deploy\apache-tomcat-9.0.80-windows-x64.zip" -C "deploy"
+curl -L "https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.100/bin/apache-tomcat-9.0.100-windows-x64.zip" -o "deploy\apache-tomcat-9.0.100-windows-x64.zip" --create-dirs
+tar -xf "deploy\apache-tomcat-9.0.100-windows-x64.zip" -C "deploy"
 )
 
 :: Clean and install a new parabank war file
@@ -18,19 +18,19 @@ CALL mvn -Dmaven.test.skip=true clean install
 
 :: Copy the war file to apache and deploy the web server
 echo Remove potential old webapps folder...
-rmdir /s /q "deploy\apache-tomcat-9.0.80\webapps"
+rmdir /s /q "deploy\apache-tomcat-9.0.100\webapps"
 
 echo Create new webapps folder...
-mkdir "deploy\apache-tomcat-9.0.80\webapps"
+mkdir "deploy\apache-tomcat-9.0.100\webapps"
 
 echo Clean new parabank.war copy...
-xcopy /s "target\parabank.war" "deploy\apache-tomcat-9.0.80\webapps\*"
+xcopy /s "target\parabank.war" "deploy\apache-tomcat-9.0.100\webapps\*"
 
 echo Temporally set CATALINA_HOME...
-set "CATALINA_HOME=%~dp0\deploy\apache-tomcat-9.0.80"
+set "CATALINA_HOME=%~dp0\deploy\apache-tomcat-9.0.100"
 
 echo Invoke Apache Startup...
-CALL "deploy\apache-tomcat-9.0.80\bin\startup.bat"
+CALL "deploy\apache-tomcat-9.0.100\bin\startup.bat"
 
 timeout /t 30
-start chrome http://localhost:8080/parabank
+start chrome http://localhost:100100/parabank
